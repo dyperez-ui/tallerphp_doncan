dec572ac6bc33eb1244050a89ad9c1ed5485d087
Dieguito0ooo <jdrodriguezg@uniboyaca.edu.co>
2025-09-25 16:28:12 -0500

    Maquillando

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Calcular Media Mediana Moda</title>
+    <link rel="stylesheet" href="STYLE.CSS">
 </head>
 <body>
 <form action="index.php" method="post">
@@ -16,7 +17,7 @@ if ($_POST && isset($_POST["txtcant"])) {
     $cant = intval($_POST["txtcant"]);
     $i = 1;
     ?>
-    <form action="prom.php" method="post">
+    <form  method="post">
     <input type="hidden" name="txtcant" value="<?php echo $cant; ?>">
     <?php
     while ($i <= $cant) {
@@ -31,4 +32,31 @@ if ($_POST && isset($_POST["txtcant"])) {
 </form>
 <?php
 }
+?>
+
+<!--Calcular promedio
+<?php 
+if($_POST && isset($_POST["txtcant"])){
+    $cant = intval($_POST["txtcant"]);
+    $j = 1;
+    $suma = 0;
+ 
+    if ($cant > 0) {
+        while($j <= $cant){
+            if (isset($_POST["txtNum".$j]) && is_numeric($_POST["txtNum".$j])) {
+                $suma += $_POST["txtNum".$j];
+            }
+            $j++;
+        }
+
+        $prom = $suma / $cant;
+        echo "EL promedio es: ". $prom;
+    } else {
+        echo "Cantidad no válida.";
+    }
+
+}else{
+    echo "NO Permitido";
+}
+
 ?>
